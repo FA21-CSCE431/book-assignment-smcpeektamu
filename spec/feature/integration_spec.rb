3457973b8baefad5c55212784600593105d99ab9
root <root@localhost>
2021-10-01 01:28:26 +0000

    updated tests

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -9,4 +9,15 @@ RSpec.describe 'Creating a book', type: :feature do
     visit books_path
     expect(page).to have_content('harry potter')
   end
+  
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'The Catcher in the Rye'
+	fill_in 'Author' with: 'JD Salinger'
+	fill_in 'Price' with: '7.43'
+	fill_in 'Publication Date' with '1951-7-16'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('The Catcher in the Rye')
+  end
 end
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   subject do
-    described_class.new(title: 'harry potter')
+    described_class.new(title: 'The Catcher in the Rye', author: 'JD Salinger', price: '7.43', pub_date: '1951-7-16')
   end
 
   it 'is valid with valid attributes' do
@@ -14,4 +14,20 @@ RSpec.describe Book, type: :model do
     subject.title = nil
     expect(subject).not_to be_valid
   end
+  
+  it 'is not valid without an author' do
+    subject.author = nil
+    expect(subject).not_to be_valid
+  end
+  
+  it 'is not valid without a price' do
+    subject.price = nil
+    expect(subject).not_to be_valid
+  end
+  
+  it 'is not valid without a publish date' do
+    subject.pub_date = nil
+    expect(subject).not_to be_valid
+  end
+  
 end
